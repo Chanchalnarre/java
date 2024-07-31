851978044d3d5e9912c41a03e6c2488c2ae729d0
Chanchalnarre <chanchalnarre94067@gmail.com>
2024-07-31 16:55:38 +0530

    first commit

---
+++ assign2.java
@@ -0,0 +1,21 @@
+public class assign2 {
+    public static void main(String[] args){
+        int a = 10;
+        byte b = 11;
+        long c = 835987298;
+        short d = 9;
+        char e = 'e';
+        float f = 23.365f;
+        double g = 45.6979387;
+        boolean h = true;
+
+        System.out.println(a);
+        System.out.println(b);
+        System.out.println(c);
+        System.out.println(d);
+        System.out.println(e);
+        System.out.println(f);
+        System.out.println(g);
+        System.out.println(h);
+    }
+}
